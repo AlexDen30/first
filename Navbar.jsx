106e50280425877043dfbae2472229d71a44cf8d
Alexander Denisov <pmax45487@gmail.com>
2020-09-23 22:54:52 +0400

    first commit

---
+++ Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="nav">
+        <div>
+          profile
+        </div>
+        <div>
+          Msg
+        </div>
+        <div>
+          News
+        </div>
+        <div>
+          Music
+        </div>
+        <div>
+          Settings
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar;
+asdasdasdasd
